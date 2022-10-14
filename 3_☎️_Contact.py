342da5cc1e15f3f45e9cf3e14010732d96a894b7
Anant-83 <113686236+Anant-83@users.noreply.github.com>
2022-10-14 18:08:08 +0530

    Add files via upload

---
+++ 3_☎️_Contact.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Contact",
+    page_icon='☎️',
+    layout="wide"
+)
+
+st.title("Hello")
+st.markdown("<p style='text-align: justify;'>I am Anant Prajapati, Currently persuing IT engineering.</p>",unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify;'>I am passionate about DSA and Competitive programming.</p>",unsafe_allow_html=True)
+st.header("Connect with me")
+st.write("E-mail :- anantprajapati8323@gmail.com")
+st.write("My Linkedin :- [Linkedin](https://www.linkedin.com/in/anant-prajapati-3586b5218/)")
