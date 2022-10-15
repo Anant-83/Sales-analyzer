dc2963467f75d5f5ff6b7a57ddfb036589550df2
Anant-83 <113686236+Anant-83@users.noreply.github.com>
2022-10-15 13:49:59 +0530

    Update 3_☎️_Contact.py

--- pages/3_☎️_Contact.py
+++ pages/3_☎️_Contact.py
@@ -8,8 +8,9 @@ st.set_page_config(
 )
 
 st.title("Hello")
-st.markdown("<p style='text-align: justify;'>I am Anant Prajapati, Currently persuing IT engineering.</p>",unsafe_allow_html=True)
-st.markdown("<p style='text-align: justify;'>I am passionate about DSA and Competitive programming.</p>",unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify;'>I am Anant Prajapati, Currently persuing IT engineering,passionate about DSA and Competitive programming.</p>",unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify;'>If you like this website , you can share this to others</p>",unsafe_allow_html=True)
+st.markdown("<p style='text-align: justify;'>Thanks for using SALES ANALYZER</p>",unsafe_allow_html=True)
 st.header("Connect with me")
 st.write("E-mail :- anantprajapati8323@gmail.com")
 st.write("My Linkedin :- [Linkedin](https://www.linkedin.com/in/anant-prajapati-3586b5218/)")
